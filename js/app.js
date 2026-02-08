8bb7fca8b086e59b60bd5a0891baa6af7d77a8ea
Alexandre Wahl <alexandre.wahl12@gmail.com>
2026-02-07 23:24:42 -0300

    feat: adicionado confirmaçao de ação ao alugar ou devolver jogo

--- js/app.js
+++ js/app.js
@@ -2,14 +2,28 @@ function alterarStatus(numeroId){
     let jogo = document.getElementById(`game-${numeroId}`);
     let botao = jogo.querySelector('.dashboard__item__button');
     let imagem = jogo.querySelector('.dashboard__item__img');
+    
+
+
 
     if (botao.textContent === 'Alugar'){
-        botao.textContent = 'Devolver';
-        botao.classList.add('dashboard__item__button--return');
-        imagem.classList.add('dashboard__item__img--rented');
+        let confirmarAcao = confirm('Voce deseja realmente alugar o jogo?')
+        if (confirmarAcao == true){
+            botao.textContent = 'Devolver';
+            botao.classList.add('dashboard__item__button--return');
+            imagem.classList.add('dashboard__item__img--rented');
+        }else{
+            return;
+        }
+        
     } else {
-        botao.textContent = 'Alugar';
-        botao.classList.remove('dashboard__item__button--return');
-        imagem.classList.remove('dashboard__item__img--rented');
+        let confirmarAcao = confirm('Voce deseja realmente devolver o jogo?');
+        if (confirmarAcao == true){
+            botao.textContent = 'Alugar';
+            botao.classList.remove('dashboard__item__button--return');
+            imagem.classList.remove('dashboard__item__img--rented');
+        }else{
+            return;
+        }
     }
 }
